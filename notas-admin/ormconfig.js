59972553c146fa7bc990daaf94e3c44d9732e348
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2023-01-04 00:03:57 -0300

    🔥 Add k8s files, ajusts in db config to prd and configure nginx proxy

--- notas-admin/ormconfig.js
+++ notas-admin/ormconfig.js
@@ -1,6 +1,8 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
-module.exports = [{
+const isProd = process.env.ENVIRONMENT === 'PROD'
+
+const configDev = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -15,4 +17,32 @@ module.exports = [{
     entitiesDir: './src/entity',
     migrationsDir: './src/database/migrations',
   }
-}];
+}
+
+const configProd = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    },
+    connectionLimit: 5
+  },
+  // Add PATH in Server
+  entities: ['./src/entity/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    entitiesDir: './src/entity',
+    migrationsDir: './src/database/migrations',
+  }
+}
+
+const config = isProd ? configProd : configDev
+
+module.exports = [config];
--- notas-aluno/src/config/database.ts
+++ notas-aluno/src/config/database.ts
@@ -1,10 +1,29 @@
 import { Pool } from 'pg';
 import { databaseName, hostDB, passwordDB, portDB, usernameDB } from '.'
 
-export const pool: Pool = new Pool({
+const isProd = process.env.ENVIRONMENT === 'PROD'
+
+const configDev = {
   host: hostDB,
   port: portDB,
   user: usernameDB,
   password: passwordDB,
   database: databaseName,
-});
+}
+
+const configProd = {
+  host: hostDB,
+  port: portDB,
+  user: usernameDB,
+  password: passwordDB,
+  database: databaseName,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  idleTimeoutMillis: 10000,
+  max: 5,
+}
+
+const config = isProd ? configProd : configDev
+
+export const pool: Pool = new Pool(config);
