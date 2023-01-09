b9d56c244f9c1d6bb4cb827afce5c93e71468c54
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2023-01-09 10:30:25 -0300

    feat: add cors open

--- notas-aluno/src/app.ts
+++ notas-aluno/src/app.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
+import cors = require("cors");
 import routes from './routes';
 
 const app: express.Express = express();
 app.use(express.json());
+app.use(cors<express.Request>({ origin: '*' }));
 app.use(routes);
 
 export default app;
